5f4a5df63f4a363035c825a1c2c9bee28e28f2a0
hui-liu <liuhui.bfu@gmail.com>
2017-01-20 21:06:55 +0800

    Create Reverse_complement_dna.py

---
+++ Scripts/Reverse_complement_dna.py
@@ -0,0 +1,69 @@
+import sys
+
+complement_table = {
+'A': 'T',
+'B': 'V',
+'C': 'G',
+'D': 'H',
+'G': 'C',
+'H': 'D',
+'M': 'K',
+'N': 'N',
+'R': 'Y',
+'S': 'S',
+'T': 'A',
+'U': 'A',
+'V': 'B',
+'W': 'W',
+'X': 'X',
+'Y': 'R',
+'a': 't',
+'b': 'v',
+'c': 'g',
+'d': 'h',
+'g': 'c',
+'h': 'd',
+'m': 'k',
+'n': 'n',
+'r': 'y',
+'s': 's',
+'t': 'a',
+'u': 'a',
+'v': 'b',
+'w': 'w',
+'x': 'x',
+'y': 'r'
+}
+
+def pqrse_fasta(seqs):
+    new_seqs = {}
+    for line in seqs:
+        if line.startswith(">"):
+            name = line.rstrip()
+            new_seqs[name] = ""
+        else:
+            new_seqs[name] = new_seqs[name] + line.rstrip()
+    return new_seqs
+
+def rev_comp(seq):
+    new_seq = []
+    line = seq.rstrip()
+    for letter in line:
+        complement_letter = complement_table[letter]
+        new_seq.append(complement_letter)
+    new_seq.reverse()
+    return "".join(new_seq)
+
+
+in_file = open(sys.argv[1])
+out_file = open(sys.argv[2], 'w')
+
+seqs = pqrse_fasta(in_file)
+
+for name in seqs.keys():
+    if name.endswith("-"):
+        print >> out_file, name + '\n' + rev_comp(seqs[name])
+    elif name.endswith("+"):
+        print >> out_file, name + '\n' + seqs[name]
+    else:
+        print >> out_file, name + '\n' + rev_comp(seqs[name]) # 如果文件没有 '+' 或 '-' 号标记正负链，则默认为负链。
