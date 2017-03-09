eef1380a532c6c592e22b2c60ecd9b60eae5c8cb
hui-liu <liuhui.bfu@gmail.com>
2017-03-09 11:18:14 +0800

    Update concatenate_multiple_rows_into_single_line.py
    Concatenating multiple rows into single line in pfam output file

--- Scripts/Python/concatenate_multiple_rows_into_single_line.py
+++ Scripts/Python/concatenate_multiple_rows_into_single_line.py
@@ -4,37 +4,18 @@
 #EMail: liuhui@bjfu.edu.cn	
 #Description: concatenate multiple rows into single line
 #Usage: python concatenate_multiple_rows_into_single_line.py inputfile outputfile
+#example: python concatenate_multiple_rows_into_single_line.py pfam.txt pfam_out.txt 
 import sys
 
-#TRINITY_GG_428_c0_g1_i1_orf1 PF13499.1 EF_hand_5
-#TRINITY_GG_428_c0_g1_i1_orf1 PF00036.27 efhand
-#TRINITY_GG_428_c0_g1_i1_orf1 PF13405.1 EF_hand_4
-#TRINITY_GG_428_c0_g1_i1_orf1 PF13833.1 EF_hand_6
-#TRINITY_GG_428_c0_g1_i1_orf1 PF13202.1 EF_hand_3
-#TRINITY_GG_429_c0_g1_i1_orf1 PF00156.22 Pribosyltran
-#TRINITY_GG_431_c5_g1_i1_orf1 PF00475.13 IGPD
-#TRINITY_GG_461_c0_g1_i1_orf1 PF01208.12 URO-D
-#TRINITY_GG_461_c0_g1_i1_orf1 PF12876.2 Cellulase-like
-
-import re
-
-out_lines = []
-with open(sys.argv[1], 'r') as f:
-    key = None
-    key_lines = []
-    for line in f:
-        m = re.match(r'^(\S+)\s(.+)$', line)
-        k, v = m.group(1), m.group(2)
-        if k != key:
-            if key:
-                out_lines.append('{0} {1}'.format(key, ' | '.join(key_lines)))
-            key = k
-            key_lines = [v]
+dict = {}
+with open(sys.argv[1], 'r') as IN:
+    for line in IN:
+        s = line.split()
+        if s[0] not in dict:
+            dict[s[0]] = s[1:]
         else:
-            key_lines.append(v)
-    else:
-        if key:
-            out_lines.append('{0} {1}'.format(key, ' | '.join(key_lines)))
+            dict[s[0]] = dict[s[0]] + ['|'] + s[1:]
 
-with open(sys.argv[2], 'w') as f:
-    f.write('\n'.join(out_lines) + "\n")
+with open(sys.argv[2], 'w') as OUT:
+    for k in dict:
+        OUT.write("%s %s\n" % (k, " ".join(dict[k])))
