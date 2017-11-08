97b3fa2916192404ccb7c78af0233053452230b1
Hui Liu <liuhui.bfu@gmail.com>
2017-11-08 21:03:12 +0800

    Update orthomclBlastPaster.py

--- Scripts/Python/orthomclBlastPaster.py
+++ Scripts/Python/orthomclBlastPaster.py
@@ -10,7 +10,6 @@ output:
 percent_match is computed by counting the length of the shorter sequence (query or subject) and dividing by the length of *that shorter sequence*
 """
 
-
 def splitEvalue(string):
     mant = None
     exp = None
@@ -29,25 +28,36 @@ def formatDigit(value):
         value = value
     return value
 
+ident_cutoff = 30
+conve_cutoff = 70
 
 IN = open(sys.argv[1], 'r')
 OUT = open(sys.argv[2], 'w')
 
 for line in IN:
     lsplit = line.split()
-    qlen, slen = int(lsplit[1]), int(lsplit[3])
-    qstar, qend = [int(lsplit[10]), int(lsplit[11])] if int(lsplit[10]) < int(lsplit[11]) else [int(lsplit[11]), int(lsplit[10])]
-    sstar, send = [int(lsplit[12]), int(lsplit[13])] if int(lsplit[12]) < int(lsplit[13]) else [int(lsplit[13]), int(lsplit[12])]
-    mant, exp = splitEvalue(lsplit[14])
     pident = str(Decimal(lsplit[5]).quantize(Decimal('0.0')))
-    if qlen >= slen:
-        match = send - sstar + 1
-        percent_match = str(Decimal(str(match / float(slen) * 100)).quantize(Decimal('0.0')))
-        OUT.write("\t".join([lsplit[0], lsplit[2], lsplit[0].split("|")[0], lsplit[2].split("|")[0], mant, exp, formatDigit(pident), formatDigit(percent_match)]) + "\n")
+    if pident < ident_cutoff:
+        continue
     else:
-        match = qend - qstar + 1
-        percent_match = str(Decimal(str(match / float(qlen) * 100)).quantize(Decimal('0.0')))
-        OUT.write("\t".join([lsplit[0], lsplit[2], lsplit[0].split("|")[0], lsplit[2].split("|")[0], mant, exp, formatDigit(pident), formatDigit(percent_match)]) + "\n")
+        qlen, slen = int(lsplit[1]), int(lsplit[3])
+        qstar, qend = [int(lsplit[10]), int(lsplit[11])] if int(lsplit[10]) < int(lsplit[11]) else [int(lsplit[11]), int(lsplit[10])]
+        sstar, send = [int(lsplit[12]), int(lsplit[13])] if int(lsplit[12]) < int(lsplit[13]) else [int(lsplit[13]), int(lsplit[12])]
+        mant, exp = splitEvalue(lsplit[14])
+        if qlen >= slen:
+            match = send - sstar + 1
+            percent_match = str(Decimal(str(match / float(slen) * 100)).quantize(Decimal('0.0')))
+            if percent_match < conve_cutoff:
+                continue
+            else:
+                OUT.write("\t".join([lsplit[0], lsplit[2], lsplit[0].split("|")[0], lsplit[2].split("|")[0], mant, exp, formatDigit(pident), formatDigit(percent_match)]) + "\n")
+        else:
+            match = qend - qstar + 1
+            percent_match = str(Decimal(str(match / float(qlen) * 100)).quantize(Decimal('0.0')))
+            if percent_match < conve_cutoff:
+                continue
+            else:
+                OUT.write("\t".join([lsplit[0], lsplit[2], lsplit[0].split("|")[0], lsplit[2].split("|")[0], mant, exp, formatDigit(pident), formatDigit(percent_match)]) + "\n")
 
 IN.close()
 OUT.close()
