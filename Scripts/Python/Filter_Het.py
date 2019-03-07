f5d951a0dbae0202eba5820df4909c5079ffab04
Hui Liu <liuhui.bfu@gmail.com>
2019-03-07 21:11:38 +0100

    Update Filter_Het.py

--- Scripts/Python/Filter_Het.py
+++ Scripts/Python/Filter_Het.py
@@ -5,22 +5,24 @@ import gzip
 # Author: Hui Liu, liuhui@bjfu.edu.cn
 
 """
-citation:
 cyvcf2: fast, flexible variant analysis with Python
 """
 
+inFile = sys.argv[1]
+cutoff = float(sys.argv[2])
 
 TetThreshod = 0.7
-input = sys.argv[1]
+if TetThreshod != cutoff:
+    TetThreshod = cutoff
 
 # header
-for line in gzip.open(input, 'r'):
+for line in gzip.open(inFile, 'r'):
     if line[0] == "#":
         print line.rstrip("\n")
     else: continue
 
 # filter high het rate locis
-vcf = VCF(input)
+vcf = VCF(inFile)
 for variant in vcf:
     HetGenos = 0
     TotGeno = 0
@@ -28,6 +30,9 @@ for variant in vcf:
     # gt_types is array of 0,1,2,3==HOM_REF, HET, UNKNOWN, HOM_ALT
     HetGenos = np.sum(variant.gt_types == 1)
     TotGeno = np.sum(variant.gt_types != 2)
-    HetRate = HetGenos / float(TotGeno)
-    if HetRate > TetThreshod:
+    if TotGeno == 0:
         print str(variant).rstrip("\n")
+    else:
+        HetRate = HetGenos / float(TotGeno)
+        if HetRate > TetThreshod:
+            print str(variant).rstrip("\n")
