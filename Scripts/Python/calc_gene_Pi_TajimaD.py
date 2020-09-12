1945056663235291ef1b83c246471494bedcec83
Hui Liu <liuhui.bfu@gmail.com>
2020-09-12 16:46:57 +0200

    Update calc_gene_Pi_TajimaD.py

--- Scripts/Python/calc_gene_Pi_TajimaD.py
+++ Scripts/Python/calc_gene_Pi_TajimaD.py
@@ -49,7 +49,7 @@ gene_Pi = N_mismatches / N_pairs
 # usage: python calc_gene_Pi_TajimaD.py Potra2n1c1.vcf.gz Potra2n1c1 ../annotation/Potra02_chr_intron.gff3
 
 import sys
-from math import sqrt
+import math
 import gzip
 
 def parseVCF(file):
@@ -65,15 +65,21 @@ def parseVCF(file):
                 if lsp[0] == "#CHROM":
                     N_indvs = (len(lsp) - 9)
                 else:
-                    pos.append([lsp[0], lsp[1]])
+                    pos.append(int(lsp[1]))
+                    REF, ALT = lsp[3], lsp[4]
+                    if "," in REF or "," in ALT: continue
                     alleles = [x.split(":")[0] for x in lsp[9:]]
                     ref = alleles.count("0/0") + alleles.count("0|0")
                     alt = alleles.count("1/1") + alleles.count("1|1")
-                    het = alleles.count("0/1") + alleles.count("1/0") + alleles.count("0|1") + alleles.count("1|0")
+                    het = alleles.count("0/1") + alleles.count("0|1")
                     ac = alt * 2 + het
                     an = 2 * (ref + alt + het)
-                    AC.append(ac)
-                    AN.append(an)
+                    #mis = 2 * N_indvs - an
+                    if ref == N_indvs:
+                        continue
+                    else:
+                        AC.append(ac)
+                        AN.append(an)
     return pos, AC, AN, N_indvs
 
 def parseGFF(file):
@@ -101,10 +107,78 @@ def geneLen(file):
                 res[gene] = end - start + 1
     return res
 
-def TajimaD(pi, S, n):
+def locateBin(a, b):
+    result = True
+    if int(a) < int(b[0]) or int(b[1]) < int(a):
+        result = False
+    return result
+
+def site_Pi(AC, AN, pos):
+    #test the --site-pi of vcftools for a single gene
+    site_Pi = []
+    for i, x in enumerate(AC):
+        y = AN[i]
+        pairs = y * (y-1)
+        mismatches = x * (y - x) + (y - x) * x
+        pi = mismatches / float(pairs)
+        site_Pi.append(pos[i]+[pi])
+    return site_Pi
+
+def vcftools_window_pi(AC, AN, pos, N_chr):
+    # test the --window-pi of vcftools for a single gene
+    win = 10000
+    START = int(math.floor(pos[0] / float(win))) * win
+    END = int(math.ceil(pos[-1] / float(win))) * win
+    window_pi_calc = [[i+1, i+win, [], [], []] for i in range(START, END, win)]
+    for i, x in enumerate(pos):
+        for j in window_pi_calc:
+            if locateBin(x, j):
+                j[2].append(x)
+                j[3].append(AC[i])
+                j[4].append(AN[i])
+    window_pi = []
+    for i in window_pi_calc:
+        N_VARIANTS = len(i[2])
+        PI = win_Pi(i[3], i[4], N_chr, win)
+        window_pi.append([i[0], i[1], N_VARIANTS, PI])
+    return window_pi
+
+
+def win_Pi(AC, AN, N_chr, win):
+    # calculate Pi for a window
+    N_variant_site_pairs = sum([x*(x-1) for x in AN])
+    N_polymorphic_sites = len(AN)
+    N_monomorphic_sites = win - N_polymorphic_sites
+    N_chr = N_indvs * 2
+    N_comparisons = (N_chr * (N_chr - 1))
+    N_pairs = N_variant_site_pairs + (N_monomorphic_sites * N_comparisons)
+
+    N_mismatches = 0
+    for i, x in enumerate(AC):
+        y = AN[i]
+        mismatches = x * (y - x) + (y - x) * x
+        N_mismatches += mismatches
+
+    Pi = N_mismatches / float(N_pairs)
+    return Pi
+
+def calc_piqi(AC, AN):
+    # https://genome.cshlp.org/content/15/11/1553.full#sec-4
+    piqi = 0.0 # pi is the derived (nonancestral) allele frequency of the ith SNP
+    for i, x in enumerate(AC):
+        y = AN[i]
+        pi = x/float(y)
+        qi = 1.0-pi
+        piqi += pi * qi
+
+    pi = 2 * piqi*N_chr/(N_chr-1.0)
+    return pi
+
+def TajimaD(AC, AN, n):
     #https://github.com/vcftools/vcftools/blob/f7aee6d26885064d834199052573e31338240b76/src/cpp/variant_file_output.cpp
     #https://github.com/AndersenLab/VCF-kit/blob/d44b597381d40b84f752b9dd7b345b1b76fa2e00/vcfkit/tajima.py
-    # Carlson et al. Genome Res (2005)
+    # Carlson et al. Genome Res (2005) https://genome.cshlp.org/content/15/11/1553.full#sec-4
+
     a1 = sum([1.0 / i for i in xrange(1, n)])
     a2 = sum([1.0 / i**2 for i in xrange(1, n)])
     n  = float(n)
@@ -114,56 +188,28 @@ def TajimaD(pi, S, n):
     c2 = b2 - ((n+2) / (a1*n)) + (a2 / (a1*a1))
     e1 = c1 / a1
     e2 = c2 / ((a1*a1) + a2)
-
+    S = len(AN)
+    pi = calc_piqi(AC, AN)
     tw = (S/a1)
     var = float(e1*S) + (e2*S*(S-1))
-    D  = (pi - tw) / sqrt(var)
+    D  = (pi - tw) / math.sqrt(var)
     return D
 
-_pos, AC, AN, N_indvs = parseVCF(sys.argv[1])
+
+# parse the input
+pos, AC, AN, N_indvs = parseVCF(sys.argv[1])
 geneid = sys.argv[2]
 gene_length_dict = geneLen(sys.argv[3])
 gene_length = gene_length_dict[geneid]
+N_chr = N_indvs * 2
 
-"""
-site_Pi = []
-for i, x in enumerate(AC):
-    y = AN[i]
-    pairs = y * (y-1)
-    mismatches = x * (y - x) + (y - x) * x
-    pi = mismatches / float(pairs)
-    site_Pi.append(_pos[i]+[pi])
-"""
+#test the --window-pi of vcftools
+#res = vcftools_window_pi(AC, AN, pos, N_chr)
+#for i in res:
+#    print i
 
-# calculate Pi for a sigle gene
-N_variant_site_pairs = sum([x*(x-1) for x in AN])
-N_polymorphic_sites = len(AN)
-N_monomorphic_sites = gene_length - N_polymorphic_sites
-N_kept_chr = N_indvs * 2
-N_comparisons = (N_kept_chr * (N_kept_chr - 1))
-N_pairs = N_variant_site_pairs + (N_monomorphic_sites * N_comparisons)
-
-N_mismatches = 0
-for i, x in enumerate(AC):
-    y = AN[i]
-    mismatches = x * (y - x) + (y - x) * x
-    N_mismatches += mismatches
-
-gene_Pi = N_mismatches / float(N_pairs)
-
-# calculate Tajima's D
-# https://genome.cshlp.org/content/15/11/1553.full#sec-4
-piqi = 0.0 # pi is the derived (nonancestral) allele frequency of the ith SNP
-for i, x in enumerate(AC):
-    y = AN[i]
-    pi = x/float(y)
-    qi = 1.0-pi
-    piqi += pi * qi
-
-pi = 2 * piqi*N_kept_chr/(N_kept_chr-1.0)
-
-gene_tajamdD = TajimaD(pi, N_polymorphic_sites, N_kept_chr)
-
+gene_Pi = win_Pi(AC, AN, N_chr, gene_length)
+gene_tajamdD = TajimaD(AC, AN, N_chr)
 
 out = open(geneid + ".tsv", "w")
 out.write("\t".join(["geneid", "Pi", "TajimaD"]) + "\n")
