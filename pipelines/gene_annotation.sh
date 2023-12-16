1cd46d29b83f17d8ca462e9ae43706167ef00564
Hui Liu <liuhui.bfu@gmail.com>
2023-12-16 13:15:53 +0800

    Update gene_annotation.sh

--- pipelines/gene_annotation.sh
+++ pipelines/gene_annotation.sh
@@ -1,7 +1,206 @@
+#########
+# EDTA
+#########
+genomedir=/media/nfs2/wbs/wbs1/working_dir/genomes/QmQl
+EDTAdir=/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl
+cd ${EDTAdir}
+
+n=0
+for i in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+n=$[n+1]
+mkdir -p ${i}
+echo "if [ \$SGE_TASK_ID -eq $n ];then"
+echo "cd ${EDTAdir}/${i}"
+echo "/media/nfs2/wbs/wbs1/bin/scripts/annotation/run_EDTA.sh \
+${genomedir}/${i}/${i}.fasta &> ${i}_EDTA.log"
+echo "fi"
+done > run_EDTA.sh
+qsub -pe mpi 1 -tc 2 -t 1-$n -o run_EDTA.out -terse -cwd -j y -V -S /bin/bash run_EDTA.sh
+
+#
+EDTAdir=/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl
+cd ${EDTAdir}
+
+for i in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+cd ${i}
+python2 ~/bin/scripts/edta_repeat2gff3.py \
+${i}.fasta.mod.EDTA.anno/${i}.fasta.mod.out ${i}.maker.gff3
+cd ..
+done
+
+########
+# split genome 
+######## 
+#split the genome by chromosome and all of the contigs were put into Contigs.fa
+EDTAdir=/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl
+cd ${EDTAdir}
+
+for i in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+cd ${i}
+python2 ~/bin/scripts/split_contigs.py \
+${i}.fasta.mod.MAKER.masked 1500000 > ${i}.ids
+cd ..
+done
+
+###########
+# make the repeatmasker gff3 file for maker
+###########
+dir=/media/nfs2/wbs/wbs1/working_dir/repeatmasker/QmQl
+EDTAdir=/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl
+cd ${dir}
+
+n=0
+for i in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+n=$[n+1]
+mkdir -p ${i}/result
+echo ">(N)n#Dummy_repeat @root  [S:25]
+nnnnnnnnnnnnnnnnnn" > ${i}/simple.lib
+echo "if [ \$SGE_TASK_ID -eq $n ];then"
+echo "cd ${dir}/${i}"
+echo "/media/nfs2/wbs/wbs1/bin/RepeatMasker-open-4-0-7/RepeatMasker \
+${EDTAdir}/${i}/${i}.fasta.mod.MAKER.masked \
+-dir result \
+-pa 48 \
+-lib simple.lib \
+-gff"
+echo "/media/nfs2/wbs/wbs1/bin/exe/python ~/bin/scripts/repeat2gff3.py \
+result/${i}.fasta.mod.MAKER.masked.out result/${i}.fasta.mod.MAKER.masked.out.gff3"
+echo "cat ../../../EDTA/QmQl/${i}/${i}.maker.gff3 \
+result/${i}.fasta.mod.MAKER.masked.out.gff3 > ${i}.repeat.gff3"
+echo "fi"
+done > run_repeatmasker.sh
+
+qsub -pe mpi 1 -tc 2 -t 1-$n -o run_repeatmasker.out -terse -cwd -j y -V -S /bin/bash run_repeatmasker.sh
+
+
+#########
+# BUSCO
+#########
+genomedir=/media/nfs2/wbs/wbs1/working_dir/genomes/QmQl
+BUSCOdir=/media/nfs2/wbs/wbs1/working_dir/BUSCO/QmQl
+cd ${BUSCOdir}
+
+n=0
+for i in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+n=$[n+1]
+mkdir -p ${i}
+echo "if [ \$SGE_TASK_ID -eq $n ];then"
+echo "cd ${BUSCOdir}/${i}"
+echo "/media/nfs2/wbs/wbs1/bin/scripts/annotation/run_busco.sh \
+${genomedir}/${i}/${i}.fasta ${i} geno &> ${i}_busco.log"
+echo "fi"
+done > run_busco.sh
+qsub -pe mpi 1 -tc 2 -t 1-$n -o run_busco.out -terse -cwd -j y -V -S /bin/bash run_busco.sh
+
+#########
+# AUGUSTUS
+#########
+cd /media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl
+n=0
+for sample in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+n=$[n+1]
+dir=/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/AUGUSTUS
+BUSCO=/media/nfs2/wbs/wbs1/working_dir/BUSCO/QmQl/${sample}/${sample}
+genome=/media/nfs2/wbs/wbs1/working_dir/genomes/QmQl/${sample}/${sample}.fasta
+echo "if [ \$SGE_TASK_ID -eq $n ];then"
+echo "mkdir -p ${dir}"
+echo "cd ${dir}"
+echo "~/bin/scripts/AUGUSTUS_train.sh ${sample} ${BUSCO} ${genome}"
+echo "fi"
+done > run_AUGUSTUS.sh
+qsub -pe mpi 1 -tc 1 -t 1-$n -o run_AUGUSTUS.out -terse -cwd -j y -V -S /bin/bash run_AUGUSTUS.sh
+
+#########
+# GeneMark
+#########
+cd /media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl
+n=0
+for sample in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+n=$[n+1]
+dir=/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/GeneMark
+genome=/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample}/${sample}.fasta.mod.MAKER.masked
+echo "if [ \$SGE_TASK_ID -eq $n ];then"
+echo "mkdir -p ${dir}"
+echo "cd ${dir}"
+echo "~/bin/scripts/GeneMark.sh ${genome}"
+echo "fi"
+done > run_GeneMark.sh
+
+qsub -l hostname=xnode01 -pe mpi 1 -tc 3 -t 1-$n -o run_GeneMark.out -terse -cwd -j y -V -S /bin/bash run_GeneMark.sh
+
+
+########
+# maker
+########
+
+cd /media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl
+# rnd1
+for sample in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+dir=/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker
+mkdir -p ${dir}
+cd ${dir}
+mkdir rnd1
+mkdir rnd2
+mkdir rnd3
+cd ${dir}/rnd1
+~/bin/scripts/QmQl/maker_rnd1.sh \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample}/${sample}.ids \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample} \
+/media/nfs2/wbs/wbs1/working_dir/rnaseq/QmQl/${sample}/${sample}_transcriptome.fasta \
+/media/nfs2/wbs/wbs1/working_dir/repeatmasker/QmQl/${sample}/${sample}.repeat.gff3 \
+${dir}/rnd1 \
+${sample}
+done
+
+# rnd2
+for sample in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+dir=/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker
+cd ${dir}/rnd2
+~/bin/scripts/QmQl/maker_rnd2.sh \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample}/${sample}.ids \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample} \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker/rnd1/rnd1.est2genome.gff \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker/rnd1/rnd1.protein2genome.gff \
+/media/nfs2/wbs/wbs1/working_dir/repeatmasker/QmQl/${sample}/${sample}.repeat.gff3 \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/snap/rnd1/genome.hmm \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/GeneMark/gmhmm.mod \
+${sample} \
+${dir}/rnd2
+done
+
+# rnd3
+for sample in $(ls ~/working_dir/genomes/QmQl/ | xargs)
+do
+dir=/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker
+cd ${dir}/rnd3
+~/bin/scripts/QmQl/maker_rnd3.sh \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample}/${sample}.ids \
+/media/nfs2/wbs/wbs1/working_dir/EDTA/QmQl/${sample} \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker/rnd2/rnd2.est2genome.gff \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker/rnd2/rnd2.protein2genome.gff \
+/media/nfs2/wbs/wbs1/working_dir/repeatmasker/QmQl/sample/{sample}/{sample}.repeat.gff3 \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/snap/rnd2/genome.hmm \
+/media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/GeneMark/gmhmm.mod \
+${sample} \
+${dir}/rnd3
+done
+
+######
+# An example
+#######
 sample=DD1Qm
 cd /media/nfs2/wbs/wbs1/working_dir/gene_prediction/QmQl/${sample}/maker/rnd1
-n=$(grep SGE_TASK_ID  ${sample}_rnd1.sh | wc -l)
-qsub -pe mpi 1 -tc 25 -t 1-$n -o ${sample}_rnd1.out -terse -cwd -j y -V -S /bin/bash ${sample}_rnd1.sh
+n=(grepSGETASKID(grep SGE_TASK_ID  {sample}_rnd1.sh | wc -l)
+qsub -pe mpi 1 -tc 25 -t 1-n−on -o {sample}_rnd1.out -terse -cwd -j y -V -S /bin/bash ${sample}_rnd1.sh
 
 ~/bin/scripts/QmQl/maker_rnd1_combine.sh
 
