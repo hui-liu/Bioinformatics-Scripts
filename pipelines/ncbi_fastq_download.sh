4b63bcc80e4914939613d09e0b446aa4e152b05e
Hui Liu <liuhui.bfu@gmail.com>
2024-01-19 15:15:21 +0800

    Update ncbi_fastq_download.sh

--- pipelines/ncbi_fastq_download.sh
+++ pipelines/ncbi_fastq_download.sh
@@ -2,7 +2,9 @@ for i in $(cat list.txt)
 do
 echo $i
 a=$(echo $i | cut -b 1-6)
-b=$(echo $i | cut -b 10- | xargs printf "%03d")
+# b=(echo(echo i | cut -b 10- | xargs printf "%03d")
+b=$(echo $i | cut -b 10-)
+c=$(printf "%03d" "${b#0}")
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${a}/${b}/${i}/${i}_1.fastq.gz
 wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${a}/${b}/${i}/${i}_2.fastq.gz
 done
